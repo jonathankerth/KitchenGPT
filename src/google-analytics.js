cae80aebbae0f84d442511ab694850e9721bd59a
jonathankerth <jonathanpkerth@gmail.com>
2023-08-05 16:33:18 -0700

    add google analytics

---
+++ src/google-analytics.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+
+(function () {
+	window.dataLayer = window.dataLayer || [];
+	function gtag() {
+		dataLayer.push(arguments);
+	}
+	gtag("js", new Date());
+
+	gtag("config", "G-R99LELJ49D");
+})();
